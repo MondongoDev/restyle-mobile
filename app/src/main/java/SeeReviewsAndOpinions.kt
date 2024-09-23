cd0d98c4b4e8e4378763a2163c0c853c58658475
fabriciodev26 <fabriciodev26@gmail.com>
2024-09-22 23:19:29 -0500

    feat: add US03

---
+++ app/src/main/java/SeeReviewsAndOpinions.kt
@@ -0,0 +1,19 @@
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.restyle_mobile.R
+
+class SeeReviewsAndOpinions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_reviews_and_opinions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
